9244b10435bc4923bdff475850c272349f2f6da9
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2023-08-11 10:23:05 -0400

    Refactored Instagram provider based on Copilot suggestion

---
+++ src/TagzApp.Providers.Instagram/InstagramProvider.cs
@@ -0,0 +1,60 @@
+﻿using System.Net.Mime;
+using System.Net;
+using System.Text.Json;
+using System.Web;
+using TagzApp.Common;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using TagzApp.Providers.Instagram.Models;
+
+namespace TagzApp.Providers.Instagram;
+
+public class InstagramProvider : ISocialMediaProvider
+{
+	public string Id => "INSTAGRAM";
+	public string DisplayName => "Instagram";
+	public TimeSpan NewContentRetrievalFrequency => TimeSpan.FromMinutes(1);
+
+	private readonly HttpClient _HttpClient;
+	private readonly ILogger<InstagramProvider> _Logger;
+
+	public InstagramProvider(IHttpClientFactory httpClientFactory, ILogger<InstagramProvider> logger)
+	{
+		_HttpClient = httpClientFactory.CreateClient("InstagramClient");
+		_Logger = logger;
+	}
+
+	public async Task<IEnumerable<Content>> GetContentForHashtag(Hashtag tag, DateTimeOffset since)
+	{
+		var response = await _HttpClient.GetAsync($"https://www.instagram.com/explore/tags/{tag.Text}/?__a=1");
+		if (response.StatusCode == HttpStatusCode.NotFound)
+		{
+			return Enumerable.Empty<Content>();
+		}
+
+		response.EnsureSuccessStatusCode();
+
+		var json = await response.Content.ReadAsStringAsync();
+		var options = new JsonSerializerOptions
+		{
+			PropertyNameCaseInsensitive = true
+		};
+		var result = JsonSerializer.Deserialize<InstagramSearchResult>(json, options);
+		return result.MediaList.Select(m => new Content
+		{
+			Provider = this.Id,
+			ProviderId = m.Id,
+			SourceUri = new Uri($"https://www.instagram.com/p/{m.Shortcode}"),
+			Author = new Creator
+			{
+				ProfileUri = new Uri($"https://www.instagram.com/{m.Owner.Username}/"),
+				ProfileImageUri = new Uri(m.Owner.ProfileImageUrl),
+				DisplayName = m.Owner.Username
+			},
+			Text = HttpUtility.HtmlEncode(m.Caption),
+			Type = Common.ContentType.Image, //TODO: change if the type of content is not a photo
+			Timestamp = DateTimeOffset.FromUnixTimeSeconds(m.TakenAtTimestamp)
+		});
+	}
+}
+
---
+++ src/TagzApp.Providers.Instagram/Models/InstagramSearchResult.cs
@@ -0,0 +1,88 @@
+﻿namespace TagzApp.Providers.Instagram.Models;
+
+public class InstagramSearchResult
+{
+	public InstagramSearchResultData Graphql { get; set; }
+
+	public IEnumerable<InstagramMedia> MediaList { get => Graphql.Hashtag.MediaList; }
+
+}
+
+public class InstagramSearchResultData
+{
+	public InstagramHashtag Hashtag { get; set; }
+}
+
+public class InstagramHashtag
+{
+	public InstagramMediaConnection EdgeHashtagToMedia { get; set; }
+
+	public InstagramMediaConnection EdgeHashtagToTopPosts { get; set; }
+
+	public IEnumerable<InstagramMedia> MediaList { get => EdgeHashtagToMedia.Edges.Select(x => x.Node); }
+}
+
+public class InstagramMediaConnection
+{
+	public IEnumerable<InstagramMediaEdge> Edges { get; set; }
+}
+
+public class InstagramMediaEdge
+{
+	public InstagramMedia Node { get; set; }
+}
+
+public class InstagramMedia
+{
+	public string Id { get; set; }
+
+	public string Shortcode { get; set; }
+
+	public long TakenAtTimestamp { get; set; }
+
+	public InstagramCaption Caption { get; set; }
+
+	public string ThumbnailUrl { get; set; }
+
+	public IEnumerable<InstagramImage> ThumbnailResources { get; set; }
+
+	public InstagramUser Owner { get; set; }
+}
+
+public class InstagramCaption
+{
+	public IEnumerable<InstagramCaptionEdge> Edges { get; set; }
+
+	public string Text { get => Edges.First().Node.Text; }
+}
+
+public class InstagramCaptionEdge
+{
+	public InstagramCaptionNode Node { get; set; }
+}
+
+public class InstagramCaptionNode
+{
+	public string Text { get; set; }
+}
+
+public class InstagramUser
+{
+	public string Id { get; set; }
+
+	public string Username { get; set; }
+
+	public string ProfileImageUrl { get; set; }
+
+	public Uri ProfileImageUri { get => new Uri(ProfileImageUrl); }
+}
+
+public class InstagramImage
+{
+	public int Width { get; set; }
+
+	public int Height { get; set; }
+
+	public string Url { get; set; }
+}
+
